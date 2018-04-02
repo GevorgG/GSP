880aac29d710265ed80041f195a57eff40f72a47
gorians <gor.abovyan@gmail.com>
2018-04-03 01:03:45 +0400

    Made changes in ClientAPI project.

---
+++ OmniSenseNetwork.GSP.ClientAPI/Models/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace OmniSenseNetwork.GSP.ClientAPI.Models
+{
+    [DataContract]
+    public class Credentials
+    {
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
